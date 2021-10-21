0f3740ff036f99894e8395ff0c3406d076adc296
devruhul <ruhulamin262001@gmail.comm>
2021-10-22 01:45:56 +0600

    finally

--- src/Pages/Home/NotFound/NotFound.js
+++ src/Pages/Home/NotFound/NotFound.js
@@ -1,18 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import notFound from "../../../images/notfound.jpg";
 
 const NotFound = () => {
-   return (
-      <div>
-         <img src={notFound} className="img-fluid w-100" alt="" />
-         <a href="/home">
-            <div className="text-center">
-               <button className="btn btn-warning"> Go Back Home </button>
-            </div>
-         </a>
-      </div>
-   );
+  return (
+    <div>
+      <img src={notFound} className="img-fluid w-100" alt="" />
+      <Link to="/home">
+        <div className="text-center">
+          <button className="btn btn-warning"> Go Back Home </button>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
